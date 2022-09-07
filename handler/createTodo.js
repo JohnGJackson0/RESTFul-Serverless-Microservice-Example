aba7948d06a1224ceba313dbacd1e4a10ec9ccc8
johngjackson0 <johngjackson0@gmail.com>
2022-09-07 13:59:40 -0400

    feat:Create todo

--- handler/createTodo.js
+++ handler/createTodo.js
@@ -1,11 +1,50 @@
-// const AWS = require("aws-sdk");
-// sls invoke local --function create
-// const TODO_TABLE = process.env.TODO_TABLE;
-// const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+const AWS = require("aws-sdk");
+const TODO_TABLE = process.env.TODO_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+const uuid = require("uuid");
 
-exports.createTodo = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "testing" }),
+/*
+  Example: 
+
+  curl --location --request POST 'https://3quh2ijg3e.execute-api.us-east-1.amazonaws.com/dev/todos' \
+  --header 'Content-Type: application/json' \
+  --data-raw '{
+      "todo": "Test"
+  }'
+*/
+
+module.exports.createTodo = async (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  if (typeof data.todo !== "string") {
+    console.error("Validation failed");
+    return;
+  }
+
+  const params = {
+    TableName: TODO_TABLE,
+    Item: {
+      id: uuid.v1(),
+      todo: data.todo,
+      checked: false,
+      createdAt: timestamp,
+      upatedAt: timestamp,
+    },
   };
+
+  try {
+    await dynamoDbClient.put(params).promise();
+  } catch (e) {
+    console.error('error putting into dynamoDB', e);
+    callback(new Error('error', e));
+    return;
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+
+  callback(null, response);
 };
